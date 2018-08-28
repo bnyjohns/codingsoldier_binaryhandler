4801993a356fad9be6ec0ba873a600ae9bb72f1b
Boney Johns <bnyjohns@gmail.com>
2018-08-28 15:16:08 +0530

    initial commit

---
+++ handler.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+exports.handler = (event, context, callback) => {
+    const fileName = event.fileName.toLowerCase();
+    let filePath;    
+    switch(fileName){
+        case "favicon.ico":
+        filePath = "./favicon.ico";
+        break;
+
+        case "soldier.png":
+        filePath = "./images/soldier.png";
+        break;
+
+        default:
+        filePath = `./certificates/${fileName}.pdf`;
+        break;
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if(err)
+            callback(err, null);
+        else
+            callback(null, data.toString('base64'));
+    });
+}
